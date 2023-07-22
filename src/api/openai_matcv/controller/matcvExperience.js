3883576919eff7d6197c6e2ceb4a1b05dbaf03ea
alexander <castro.t.alex@gmail.com>
2023-07-22 11:39:30 -0500

    adding two endpoints for  the matcv backend

--- src/api/openai_matcv/controller/matcvExperience.js
+++ src/api/openai_matcv/controller/matcvExperience.js
@@ -6,8 +6,8 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 module.exports = {
-  async generateResponse(ctx) {
-    console.log("entre aqui en matcv profile");
+  async generateExperience(ctx) {
+    console.log("entre aqui en experience");
     const {
       role,
       market,
@@ -20,13 +20,16 @@ module.exports = {
       const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: `
-       You are a Human Resources Manager, you are here to assist me in crafting an impressive Curriculum Vitae (CV), you can also recive auto which means that you have to detect the language, in this case you
-        will be helping me to create a profile description based on the following information, Don't add references information and the note:
+       You are a Human Resources Manager, you are here to assist me in crafting an impressive Curriculum Vitae (CV),you can also recive auto which means that you have to detect the language, in this case you
+        will be helping me to create a Job experience description based on the following information, Don't add references information and the note:
      
        --
-       input: role: {Web development}, market: {Tech}, keywords: {nextjs, react, taiwind, nodejs, express} in {english}
-       Correct ouput:Experienced web developer specializing in the tech sector, proficient in Next.js, React, Tailwind, Node.js, and Express. 
-       Skilled in creating dynamic and responsive websites, optimizing user experiences, and delivering high-quality solutions.
+       input: role: {Team leader}, market: {digital marketing}, keywords: {leader, front end, backend - strapi} in {english}
+       Correct ouput:I led the digital marketing team, supervising and coordinating the marketing strategies to drive the growth of the company.
+       Developed and designed the user interface (front-end) of the company's website, using technologies such as HTML, CSS and JavaScript.
+       I implemented the backend functionality of the website using Strapi, an open source CMS.
+       Directed digital marketing campaigns to increase brand visibility and generate qualified leads.
+       Analyzed and optimized the performance of digital marketing campaigns by tracking metrics and analyzing data.
        --
         input: role: ${role}, market: ${market}, keywords: ${keywords} in ${language}
         Correct ouput:`,
