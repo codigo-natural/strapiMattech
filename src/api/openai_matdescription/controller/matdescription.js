fffe85c595228ba09c7a12b3991d7c8a7fdc9db1
alexander <castro.t.alex@gmail.com>
2023-09-17 22:28:38 -0500

    Fixing the language and the spaces bug

--- src/api/openai_matdescription/controller/matdescription.js
+++ src/api/openai_matdescription/controller/matdescription.js
@@ -1,18 +1,9 @@
 const { Configuration, OpenAIApi } = require("openai");
-// const crypto = require("crypto");
 
 const configuration = new Configuration({
   apiKey: process.env.OPEN_AI_TOKEN,
 });
 const openai = new OpenAIApi(configuration);
-
-const socialMediaKeywords = {
-  facebook: ["facebook", "fb"],
-  twitter: ["twitter", "tw"],
-  instagram: ["instagram", "ig", "insta"],
-  telegram: ["telegram", "tg"],
-  whatsapp: ["whatsapp", "wa"],
-};
 
 module.exports = {
   async generateResponse(ctx) {
@@ -28,10 +19,11 @@ module.exports = {
     try {
       const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `Instruction: Generate creative ad copy without additional spaces at the beginning for different social media platforms and languages.
+        prompt: `Instruction: Generate creative advertisement copy without additional spaces at the beginning for different social media networks, only use the language that the user send for all the response text generated.
 
-        Context: Optimized text is required for each social network without extra spaces at the beginning, regardless of language.
-        
+        Context: At the beginning of each social network you must include the contraction of the name of the social network, for example: "FB:" for Facebook, "TW:" for Twitter, "IG:" for Instagram, "TG:" for Telegram, "WA:" for WhatsApp avoid reapeting the contraction of the social network in the text.
+        Optimized text is required for each social network without extra spaces at the beginning, regardless of language.
+
         examples:
         
         input: "Give me a copywriting idea for ["instagram", "twitter"] advertising in {english}. {ColSanitas} is a {healthcare-focused} company that take care of {health}"
@@ -40,13 +32,12 @@ module.exports = {
         input: "Give me a copywriting idea for ["facebook"] advertising in {english}. {XfarmV} is a {retail} company that sells {precision agriculture technologies}"
         output: "FB:Elevate your farming journey with XfarmV's AgroAdvance. 🚀Experience cutting-edge solutions tailored for the modern farmer🚜, delivering exceptional results and driving agricultural success🌱. #AgroAdvance #FarmingInnovation"
         
-        input: "Give me a copywriting idea for ["twitter"] advertising in {auto}. {TechCo} is a {technology-focused} company that sells {Software and Applications}"
+        input: "Give me a copywriting idea for ["twitter"] advertising in {english}. {TechCo} is a {technology-focused} company that sells {Software and Applications}"
         output: "TW:Experience the Future of Technology with {TechCo}'s Advanced Software Solutions💻. Discover a world of possibilities with our diverse range of software applications tailored to meet your needs. 🔒From secure solutions to intuitive interfaces, {TechCo} empowers you to transform the way you work and play. 🔥#TechCo #AdvancedSolutions #Innovation"
         
-        input: "Give me a copywriting idea for ["telegram"] advertising in {spanish}. {Walmart} is a {retail-focused} company that sells {everything}"
-        incorrect output: " TG: Scopri un mondo di possibilità con le nostre soluzioni software avanzate! "
-        correct output: "TG:¡📢Descubre en Walmart! Únete a nuestro canal de Telegram para las mejores ofertas, promociones y novedades. 🎉 Compra desde casa y encuentra todo lo que necesitas en un solo lugar. ¡No te pierdas nuestras exclusivas ofertas en productos de calidad a precios increíbles! Únete hoy y disfruta de una experiencia única con Walmart. ¡Te esperamos!😉"
-        
+        input: "Give me a copywriting idea for ["telegram"] advertising in {english}. {Walmart} is a {retail-focused} company that sells {everything}"
+        incorrect output: "          TG: Scopri un mondo di possibilità con le nostre soluzioni software avanzate!"
+        correct output: "TG:📢Discover at Walmart! Join our Telegram channel for the best offers, promotions and news. 🎉 Shop from home and find everything you need in one place. Don't miss our exclusive offers on quality products at incredible prices! Join today and enjoy a unique experience with Walmart. We are waiting for you!😉"
         Remember, do not include extra spaces at the beginning in any language.
         --
         input: "Give me a copywriting idea for ${socialMedia} advertising in ${language}. ${company} is a ${field} company that sells ${productDescription}".
@@ -55,20 +46,10 @@ module.exports = {
         temperature: 0.6,
         max_tokens: 1000,
       });
-
-      let detectedSocialMedia = null;
-      for (const socialMedia in socialMediaKeywords) {
-        const keywords = socialMediaKeywords[socialMedia];
-        if (
-          keywords.some((keyword) =>
-            socialMedia.toLowerCase().includes(keyword)
-          )
-        ) {
-          detectedSocialMedia = socialMedia;
-          break;
-        }
-      }
-
+      const formattedResponse = response.data.choices[0].text.replace(
+        /^\s+/gm,
+        ""
+      );
       const data = {
         data: {
           payload_in: {
@@ -79,15 +60,13 @@ module.exports = {
             productDescription: productDescription,
           },
           payload_out: {
-            copywriting: response.data.choices[0].text.trim(),
-            // id: crypto.randomUUID(),
-            // socialMedia: socialMedia,
+            copywriting: formattedResponse,
           },
           users_permissions_user: users_permissions_user,
           Source: "MatDescription",
         },
       };
-      console.log(data.data.payload_out)
+      console.log(data.data.payload_out);
       await strapi.db.query("api::request.request").create(data);
 
       ctx.send(data.data.payload_out);
