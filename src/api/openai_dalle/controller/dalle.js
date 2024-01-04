8989150725d2a7d92f43f0f45b625a8783824e30
alexander <castro.t.alex@gmail.com>
2024-01-03 22:00:11 -0500

    improving the dalle model to a dall-2-3

--- src/api/openai_dalle/controller/dalle.js
+++ src/api/openai_dalle/controller/dalle.js
@@ -10,28 +10,38 @@ module.exports = {
     const { prompt, users_permissions_user } = ctx.request.body;
 
     try {
-      const response = await openai.createImage({
-        model: "dall-e-2", //dalle e 3 only support 1 image per request, important to change the reduce of 1 image instead of 2 in the plan
-        prompt: prompt,
-        n: 2,
-        size: "1024x1024",
-        response_format: "b64_json",
-      });
+      const responses = await Promise.all([
+        openai.createImage({
+          model: "dall-e-3", //dalle e 3 only support 1 image per request
+          prompt: prompt,
+          n: 1,
+          size: "1024x1024",
+          response_format: "b64_json",
+        }),
+        openai.createImage({
+          model: "dall-e-3", //dalle e 3 only support 1 image per request
+          prompt: prompt,
+          n: 1,
+          size: "1024x1024",
+          response_format: "b64_json",
+        }),
+      ]);
 
       // console.log("format 64", response.data.data);
+      const imageUrls = responses.map((response) => response.data.data[0]);
+
       const data = {
         data: {
           payload_in: { prompt: prompt },
-          payload_out: { resp: response.data.data },
+          payload_out: { resp: imageUrls },
           users_permissions_user: users_permissions_user,
           Source: "MatDalle",
         },
       };
-      const request = await strapi.db
-        .query("api::request.request")
-        .create(data);
 
-      ctx.send({ data: response.data.data });
+      await strapi.db.query("api::request.request").create(data);
+
+      ctx.send({ data: imageUrls });
     } catch (error) {
       if (error.response) {
         console.log(error.response.status);
